83d0dd06ea58de506dd5a7084386abc6147e1781
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-27 16:00:35 +0700

    Laravel - View Data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Ini Adalah Halaman Blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,72 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Rafly Alfarizi', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Perkembangan Teknologi AI dalam Kehidupan Sehari-hari',
+            'author' => 'Rafly Alfarizi',
+            'body' => 'Kecerdasan Buatan (AI) kini menjadi bagian penting dari kehidupan 
+            sehari-hari, mulai dari asisten virtual hingga rekomendasi film di platform streaming. 
+            Teknologi AI terus berkembang, membantu berbagai sektor seperti kesehatan, pendidikan, 
+            dan transportasi. Misalnya, AI membantu dokter mendiagnosis penyakit lebih cepat dan akurat. 
+            Di sektor transportasi, AI mendukung pengembangan kendaraan otonom yang dapat mengurangi angka 
+            kecelakaan lalu lintas. Namun, penerapan AI juga menghadapi tantangan, seperti privasi data dan 
+            etika penggunaan, yang perlu diatasi agar teknologi ini dapat dimanfaatkan secara aman dan berkelanjutan.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Tips Meningkatkan Produktivitas di Era Digital',
+            'author' => 'Rafly Alfarizi',
+            'body' => 'Di era digital, produktivitas sering kali terhambat oleh distraksi dari media sosial dan aplikasi 
+            hiburan. Untuk meningkatkan produktivitas, penting untuk membangun kebiasaan yang baik, seperti mengatur jadwal 
+            harian, menetapkan prioritas, dan menggunakan teknik seperti Pomodoro untuk fokus pada tugas dalam waktu tertentu. 
+            Penggunaan aplikasi manajemen waktu, seperti Trello atau Notion, juga bisa membantu mengatur tugas dan mengurangi 
+            kebingungan. Di samping itu, penting untuk menjaga keseimbangan antara pekerjaan dan waktu istirahat agar tetap produktif 
+            sepanjang hari.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Perkembangan Teknologi AI dalam Kehidupan Sehari-hari',
+            'author' => 'Rafly Alfarizi',
+            'body' => 'Kecerdasan Buatan (AI) kini menjadi bagian penting dari kehidupan 
+            sehari-hari, mulai dari asisten virtual hingga rekomendasi film di platform streaming. 
+            Teknologi AI terus berkembang, membantu berbagai sektor seperti kesehatan, pendidikan, 
+            dan transportasi. Misalnya, AI membantu dokter mendiagnosis penyakit lebih cepat dan akurat. 
+            Di sektor transportasi, AI mendukung pengembangan kendaraan otonom yang dapat mengurangi angka 
+            kecelakaan lalu lintas. Namun, penerapan AI juga menghadapi tantangan, seperti privasi data dan 
+            etika penggunaan, yang perlu diatasi agar teknologi ini dapat dimanfaatkan secara aman dan berkelanjutan.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Tips Meningkatkan Produktivitas di Era Digital',
+            'author' => 'Rafly Alfarizi',
+            'body' => 'Di era digital, produktivitas sering kali terhambat oleh distraksi dari media sosial dan aplikasi 
+            hiburan. Untuk meningkatkan produktivitas, penting untuk membangun kebiasaan yang baik, seperti mengatur jadwal 
+            harian, menetapkan prioritas, dan menggunakan teknik seperti Pomodoro untuk fokus pada tugas dalam waktu tertentu. 
+            Penggunaan aplikasi manajemen waktu, seperti Trello atau Notion, juga bisa membantu mengatur tugas dan mengurangi 
+            kebingungan. Di samping itu, penting untuk menjaga keseimbangan antara pekerjaan dan waktu istirahat agar tetap produktif 
+            sepanjang hari.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
