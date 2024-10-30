a0b7a2408688263d4c3a1cba2462decf9fb81e4c
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-31 00:21:32 +0700

    Laravel - Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory, Notifiable;
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
