036d5d9657c8db35860cc56c56775ca37850e58c
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-31 18:31:10 +0700

    Laravel - Post category

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Support\Str;
 use PharIo\Manifest\Author;
 use App\Models\User;
@@ -22,6 +23,7 @@ class PostFactory extends Factory
         return [
             'title' => fake()->sentence(),
             'author_id' => User::factory(),
+            'category_id' => Category::factory(),
             'slug' => Str::slug(fake()->sentence()),
             'body' => fake()->text()
         ];
