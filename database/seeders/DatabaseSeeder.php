59284990dc7f34bada955bbd7a9e47d15a8b75ed
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-11-03 15:01:14 +0700

    Laravel - Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web Design'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Dongeng si Kancil memang sangat digemari tua dan muda. Dongeng fabel dengan tokoh utama si Kancil yang cerdik ini selalu bisa menghibur pembacanya. Karakter si Kancil yang banyak akal membuat jalan cerita jadi menarik dan membuat penasaran. Dalam beberapa dongeng, Si Kancil selalu dicari ketika ada masalah yang perlu dipecahkan.'
+        // ]);
+
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
